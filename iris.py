546fd9a055d4d5ed1091847150b1353929894e79
Timoose <timokerremans@gmail.com>
2021-03-05 10:11:31 +0100

    Model training in .py file

--- iris.py
+++ iris.py
@@ -1,2 +1,27 @@
-# see jupyter notebook
-print("See model.ipynb for model creation.")
+import pandas as pd
+import numpy as np
+import pickle
+
+# read data from local csv
+df = pd.read_csv('./data/iris_csv.csv')
+df.head()
+
+# get out input and target
+X = np.array(df.iloc[:, 0:4])
+y = np.array(df.iloc[:, 4:])
+
+# one hot encode target
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+# split data into test and train
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2)
+
+# load linear regression model and actually train
+from sklearn.svm import SVC
+sv = SVC(kernel='linear').fit(X_train, y_train)
+
+# save trained model as serialised pickle
+pickle.dump(sv, open("iri.pkl","wb"))
