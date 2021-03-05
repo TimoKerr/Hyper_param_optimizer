8e5053389b15a6e2bf536ab129c7a8a9ca566f75
Timoose <timokerremans@gmail.com>
2021-03-05 13:45:51 +0100

    Made working pytest

--- app_test.py
+++ app_test.py
@@ -1,3 +1,8 @@
 """ Just a test file"""
 from mylib import util
 print(util.training_complete())
+
+# needs test_* function to run on pytest
+def test_function():
+    success = util.training_complete()
+    return success
